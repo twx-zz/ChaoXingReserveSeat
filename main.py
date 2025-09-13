b03b141a47ba7b207df6c65ae4c00b727966e98d
twx-zz <2301709745@qq.com>
2025-09-13 19:46:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ SLEEPTIME = 0.1  # 减少间隔时间
 RESERVE_TARGET_TIME = "22:00:00"  # 预约开始的目标时间（北京时间）
 ENABLE_SLIDER = True  # 是否有滑块验证
 MAX_ATTEMPT = 3  # 减少重试次数，专注速度
-RESERVE_NEXT_DAY = False  # 预约明天而不是今天的
+RESERVE_NEXT_DAY = True  # 预约明天而不是今天的
 CAPTCHA_POOL_SIZE = 5  # 验证码池大小
 CAPTCHA_PRELOAD_AT = "21:59:10"  # 验证码池启动时间（北京时间）
 # ==========================================

d84c96e749bf7d90991c0655934ab46e96f26739
twx-zz <2301709745@qq.com>
2025-09-17 20:09:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ get_current_dayofweek = lambda action: (
 )
 
 SLEEPTIME = 0.1  # 减少间隔时间
-RESERVE_TARGET_TIME = "22:00:00"  # 预约开始的目标时间（北京时间）
+RESERVE_TARGET_TIME = "20:11:00"  # 预约开始的目标时间（北京时间）
 ENABLE_SLIDER = True  # 是否有滑块验证
 MAX_ATTEMPT = 1  # 减少重试次数，专注速度
-RESERVE_NEXT_DAY = True  # 预约明天而不是今天的
+RESERVE_NEXT_DAY = False  # 预约明天而不是今天的
 CAPTCHA_POOL_SIZE = 5  # 验证码池大小
 CAPTCHA_PRELOAD_TIME = 5  # 提前5秒开始预加载验证码
 
